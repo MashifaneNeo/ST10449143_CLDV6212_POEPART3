280202259211987b9fc9c707f429e8ccd38ab85a
Neo Ya Gao Mashifane <ST10449143@imconnect.edu.za>
2025-11-12 23:17:39 +0200

    added authentication models for logging in and registering

---
+++ ST10449143_CLDV6212_POEPART2/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ST10449143_CLDV6212_POEPART1.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username or Email")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
